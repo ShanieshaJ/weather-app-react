125473cf8615832bc0638285303372b028284759
Shaniesha John <shaniesha.john.94@gmail.com>
2022-12-13 01:42:24 -0500

    Removed createRoot from this file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+export default function index() {
+  return (
+    <div className="index">
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
